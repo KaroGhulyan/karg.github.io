547cd0c5656a11dd8ac98769730534d7ed17c779
KarG <kghulyan93@github.com>
2020-01-31 15:36:59 +0400

    About Us Final

--- Bootstrap 4/Limitless/js/about.js
+++ Bootstrap 4/Limitless/js/about.js
@@ -1,3 +1,11 @@
 
 // Get the current year for the copyright
 $("#year").text(new Date().getFullYear());
+
+// slick
+
+$(".slider").slick({
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll: 1
+})
